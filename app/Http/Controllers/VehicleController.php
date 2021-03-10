7b07bd34fb750c3192adb1c74149903076805422
edgardegantea <edgar.degante.a@gmail.com>
2021-03-10 16:50:16 -0600

    Prueba de carga de archivos a GitHub

---
+++ app/Http/Controllers/VehicleController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vehicle;
+use Illuminate\Http\Request;
+
+class VehicleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $vehicles = Vehicle::all();
+        return view('vehicles.index', compact('vehicles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('vehicles.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $vehicle = request()->except('_token');
+        Vehicle::insert($vehicle);
+        return redirect()->to(url('/vehicles'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Vehicle  $vehicle
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vehicle $vehicle)
+    {
+        return view('vehicles.show', compact('vehicle'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Vehicle  $vehicle
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Vehicle $vehicle)
+    {
+        return view('vehicles.edit', compact('vehicle'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Vehicle  $vehicle
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Vehicle $vehicle)
+    {
+        $dataVehicle = request()->except('_token');
+        $vehicle->update($dataVehicle);
+        return redirect()->to(url('/vehicles'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Vehicle  $vehicle
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vehicle $vehicle)
+    {
+        $vehicle->delete();
+        return redirect()->to(url('/vehicles'));
+    }
+}
---
+++ resources/views/vehicles/index.blade.php
@@ -0,0 +1,59 @@
+<link rel="stylesheet" href="{{ asset('/assets/css/bootstrap.min.css') }}">
+
+<div class="container">
+<br><br>
+    <div class="card">
+        <div class="card-header">
+            <div class="row">
+                <div class="col-md-8">
+                    <h2 class="card-title">Listado de vehículos registrados en la base de datos</h2>
+                </div>
+                <div class="col-md-4">
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <a class="btn btn-primary" href="{{ route('vehicles.create') }}">+ Nuevo</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <table class="table table-striped">
+
+                <thead>
+                    <tr>
+                        <th>Vehículo</th>
+                        <th>Información</th>
+                        <th>Descripción</th>
+                        
+                    </tr>
+                    
+                </thead>
+
+                <tbody>
+                    @forelse ($vehicles as $vehicle)
+                    <tr>
+                        <td>
+                            <a class="btn btn-info btn-small" href="{{ route('vehicles.show', $vehicle->id) }}">
+                                <h4>{{ $vehicle->brand }} {{ $vehicle->model }}</h4></td>
+                            </a>
+                        <td>
+                            <p><b>Número de serie: </b>{{ $vehicle->serialNumber }}</p>
+                            <p><b>Color: </b>{{ $vehicle->color }}</p>
+                            <p><b>Puertas: </b>{{ $vehicle->numberOfDoors }}</p>
+                            <p><b>Asientos: </b>{{ $vehicle->numberOfSeats }}</p>
+                            <p><b>Matrícula: </b>{{ $vehicle->carRegistration }}</p>
+                        </td>
+                        <td><p>{{ $vehicle->description }}</p></td>
+                        
+                    @empty
+                        <h1>La tabla no tiene datos</h1>
+                    </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+      </div>
+
+
+    
+
+</div>
